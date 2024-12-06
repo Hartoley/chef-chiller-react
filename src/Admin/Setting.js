f8a79a0c4f5b95eb16a0292862dfca80ecbfd4dd
Hartoley <tolanijimoh1@gmail.com>
2024-12-06 21:10:06 +0000

    display

--- src/Admin/Setting.js
+++ src/Admin/Setting.js
@@ -10,6 +10,15 @@ const Setting = ({ showCustomAlert }) => {
   const [orders, setOrders] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const id = JSON.parse(localStorage.getItem("id"));
+  const [selectedOrder, setSelectedOrder] = useState(null);
+
+  const handleOrderClick = (order) => {
+    setSelectedOrder(order);
+  };
+
+  const closeModal = () => {
+    setSelectedOrder(null);
+  };
 
   useEffect(() => {
     socket.on("message", (message) => {
@@ -69,143 +78,107 @@ const Setting = ({ showCustomAlert }) => {
   }, [id]);
 
   return (
-    <>
-      <main className="child flex-1 p-6 bg-gray-400 w-[63.65vw]">
-        <h1 className="text-xl md:text-2xl font-bold mb-4 text-center">
-          Order History
-        </h1>
-        <section className="section1 flex items-center rounded-md justify-between mb-6 overflow-hidden">
-          <div className="bg-gray-400 w-full rounded-lg shadow-lg">
-            <section className="p-4 rounded-md">
-              <div className="mb-4 rounded-md">
-                {isLoading ? (
-                  <div className="overflow-y-scroll no-srollbar max-h-[70vh]">
-                    {/* Placeholder Loading State */}
-                    {[...Array(5)].map((_, index) => (
+    <main className="child flex-1 p-6 bg-gray-400 w-[63.65vw]">
+      <h1 className="text-xl md:text-2xl font-bold mb-4 text-center">
+        Order History
+      </h1>
+      <section className="section1 flex items-center rounded-md justify-between mb-6 overflow-hidden">
+        <div className="bg-gray-400 w-full rounded-lg shadow-lg">
+          <section className="p-4 rounded-md">
+            <div className="mb-4 rounded-md">
+              {isLoading ? (
+                <div className="overflow-y-scroll no-scrollbar max-h-[70vh]">
+                  {/* Placeholder Loading State */}
+                  {[...Array(5)].map((_, index) => (
+                    <div
+                      key={index}
+                      className="bg-gray-300 p-4 rounded-lg mb-4 shadow-md animate-pulse"
+                    >
+                      <h6 className="font-semibold text-gray-800">
+                        Loading...
+                      </h6>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <div className="overflow-y-scroll no-scrollbar max-h-[70vh]">
+                  {Array.isArray(orders) && orders.length > 0 ? (
+                    orders.map((order, index) => (
                       <div
                         key={index}
-                        className="bg-gray-300 p-4 rounded-lg mb-4 shadow-md animate-pulse"
+                        className="bg-gray-50 p-4 rounded-lg mb-4 shadow-md cursor-pointer"
+                        onClick={() => handleOrderClick(order)}
                       >
-                        <div className="flex justify-between text-sm">
-                          <div>
-                            <h6 className="font-semibold text-gray-800">
-                              Loading...
-                            </h6>
-                            <p className="text-gray-600">Status: Loading...</p>
-                            <p className="text-gray-600">
-                              Payment Method: Loading...
-                            </p>
-                            <p className="text-gray-600">
-                              Ordered On: Loading...
-                            </p>
-                          </div>
-                          <div className="flex flex-col items-end">
-                            <p className="text-sm text-gray-600">
-                              Date Delivered: Loading...
-                            </p>
-                          </div>
-                        </div>
+                        <p className="font-semibold text-gray-800">
+                          Order {index + 1}
+                        </p>
+                        <p className="text-gray-600">
+                          Ordered On:{" "}
+                          {new Date(order.orderedDate).toLocaleString()}
+                        </p>
                       </div>
-                    ))}
-                  </div>
-                ) : (
-                  <div className="overflow-y-scroll no-scrollbar max-h-[70vh]">
-                    {Array.isArray(orders) && orders.length > 0 ? (
-                      orders
-                        .filter(
-                          (order) =>
-                            order.status === "Payment Pending" ||
-                            order.status === "Payment Approved" ||
-                            order.status === "Payment Declined"
-                        )
-                        .map((order, index) => (
-                          <div
-                            key={index}
-                            className="bg-gray-50 p-4 rounded-lg mb-4 shadow-md"
-                          >
-                            <div className="flex flex-col gap-4">
-                              {/* Order Header: Basic Information */}
-                              <div className="flex justify-between text-sm">
-                                <div>
-                                  <h6 className="font-semibold text-gray-800">
-                                    Order {index + 1}
-                                  </h6>
-                                  <p className="text-gray-600">
-                                    Status: {order.status}
-                                  </p>
-                                  <p className="text-gray-600">
-                                    Payment Method: {order.paymentMethod}
-                                  </p>
-                                  <p className="text-gray-600">
-                                    Ordered On:{" "}
-                                    {new Date(
-                                      order.orderedDate
-                                    ).toLocaleString()}
-                                  </p>
-                                </div>
-                                <div className="flex flex-col items-end">
-                                  <p className="text-sm text-gray-600">
-                                    Date Delivered:{" "}
-                                    {new Date(
-                                      order.dateToBeDelivered
-                                    ).toLocaleDateString()}
-                                  </p>
-                                </div>
-                              </div>
+                    ))
+                  ) : (
+                    <p className="text-gray-900 text-center">
+                      No orders found.
+                    </p>
+                  )}
+                </div>
+              )}
+            </div>
+          </section>
+        </div>
+      </section>
 
-                              {/* Order Products List: List of Food Items */}
-                              <div className="overflow-y-auto no-scrollbar max-h-48">
-                                {order.products.map((product, idx) => (
-                                  <div
-                                    key={idx}
-                                    className="flex justify-between border-b py-2 text-sm"
-                                  >
-                                    <div className="flex flex-col">
-                                      <p className="font-semibold text-gray-800">
-                                        {product.productName}
-                                      </p>
-                                      <p className="text-gray-600">
-                                        Quantity: {product.quantity}
-                                      </p>
-                                      <p className="text-gray-600">
-                                        Price: ₦
-                                        {product.price.toLocaleString("en-NG", {
-                                          minimumFractionDigits: 2,
-                                          maximumFractionDigits: 2,
-                                        })}
-                                      </p>
-                                    </div>
-                                  </div>
-                                ))}
-                              </div>
+      {/* Modal for displaying full order details */}
+      {selectedOrder && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-[90vw] max-w-md">
+            <h2 className="text-lg font-bold mb-4">Order Details</h2>
+            <p className="text-sm text-gray-800">
+              Status: {selectedOrder.status}
+            </p>
+            <p className="text-sm text-gray-800">
+              Payment Method: {selectedOrder.paymentMethod}
+            </p>
+            <p className="text-sm text-gray-800">
+              Ordered On: {new Date(selectedOrder.orderedDate).toLocaleString()}
+            </p>
+            <p className="text-sm text-gray-800">
+              Date Delivered:{" "}
+              {new Date(selectedOrder.dateToBeDelivered).toLocaleDateString()}
+            </p>
 
-                              {/* Only display the action buttons for orders with 'Payment Pending' or 'Approved' */}
-                              {order.status === "Payment Pending" ||
-                              order.status === "Payment Approved" ||
-                              order.status === "Payment Declined" ? (
-                                <div className="flex justify-between mt-4">
-                                  <p className="text-sm text-gray-600">
-                                    Payment status: {order.status}
-                                  </p>
-                                </div>
-                              ) : null}
-                            </div>
-                          </div>
-                        ))
-                    ) : (
-                      <p className="text-gray-900 text-center">
-                        No orders found.
-                      </p>
-                    )}
-                  </div>
-                )}
-              </div>
-            </section>
+            <div className="mt-4 overflow-y-auto max-h-48">
+              {selectedOrder.products.map((product, idx) => (
+                <div key={idx} className="border-b py-2">
+                  <p className="font-semibold text-gray-800">
+                    {product.productName}
+                  </p>
+                  <p className="text-sm text-gray-600">
+                    Quantity: {product.quantity}
+                  </p>
+                  <p className="text-sm text-gray-600">
+                    Price: ₦
+                    {product.price.toLocaleString("en-NG", {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}
+                  </p>
+                </div>
+              ))}
+            </div>
+
+            <button
+              className="mt-4 bg-red-500 text-white py-2 px-4 rounded"
+              onClick={closeModal}
+            >
+              Close
+            </button>
           </div>
-        </section>
-        <ToastContainer /> {/* Toast notifications container */}
-      </main>
-    </>
+        </div>
+      )}
+    </main>
   );
 };
 
